6b02168dcbbebd738c5c540f26e55bce885f2b25
Andrew Wentzel <tehwentzel@gmail.com>
2021-07-16 17:09:28 -0500

    some random stuff

--- python/Constants.py
+++ python/Constants.py
@@ -4,3 +4,20 @@ class Const():
     mdasi_file = data_dir + "MDASI_72021.xlsx"
     
     camprt_dir = data_dir + "CAMPRT_Centroids/"
+    
+    tumor_aliases = {'GTV node': 'GTVn',
+                     'GTV-N': 'GTVn',
+                     'GTV_n': 'GTVn',
+                     'GTVn1': 'GTVn',
+                     'GTV primary': 'GTVp',
+                     'GTV-P': 'GTVp',
+                     'GTV_p': 'GTVp',
+                     'GTV_P': 'GTVp',
+                     'GTV P': 'GTVp',
+                     'GTV nodes': 'GTVn',
+                     'GTV-N1': 'GTVn',
+                     'GTV_N1': 'GTVn',
+                     'GTV N': 'GTVn',
+                     'GTV-NR': 'GTVn2', #I am only aware of this for 10144 and 10022, may need more robust solution later
+                     'GTV-NL': 'GTVn3'
+                     }
