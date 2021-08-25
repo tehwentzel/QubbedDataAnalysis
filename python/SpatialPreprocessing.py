2a0b8950bfc7b67d3163d85a2f62e2a46bf5c582
Andrew Wentzel <tehwentzel@gmail.com>
2021-08-25 12:59:45 -0500

    added preprocessing for symptom data

--- python/SpatialPreprocessing.py
+++ python/SpatialPreprocessing.py
@@ -348,29 +348,6 @@ def load_spatial_files(root = None):
         
     file_dict = {sid: {'doses': dose_dict.get(sid), 'distances': dist_dict.get(sid)} for sid in shared_ids}
     return file_dict
-
-def np_converter(obj):
-    #converts stuff to vanilla python  for json since it gives an error with np.int64 and arrays
-    if isinstance(obj, np.integer):
-        return int(obj)
-    elif isinstance(obj, np.float):
-        return round(float(obj),5)
-    elif isinstance(obj, np.ndarray):
-        return obj.tolist()
-    elif isinstance(obj, datetime.datetime):
-        return obj.__str__()
-    return obj
-    
-def np_dict_to_json(d,destination_file, nan_to_null = False):   
-    try:
-        with open(destination_file, 'w') as f:
-            #nan_to_null makes it save it as null in the json instead of NaN
-            #more useful when it's sent to a json but will be read back in python as None
-            simplejson.dump(d,f,default = np_converter, ignore_nan = nan_to_null)
-        return True
-    except Exception as e:
-        print(e)
-        return False
     
 def load_pdict(filepath = None):
     if filepath is None:
